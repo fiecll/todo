69bc5041aa3e4332532ab55fadb6281f849b0330
fie <doublerokko@yahoo.co.jp>
2025-05-21 10:01:31 +0900

    test

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,29 +1,36 @@
-import mongoose from "mongoose";
+import mongoose, { Connection } from 'mongoose';
 
 declare global {
   // eslint-disable-next-line no-var
-  var mongoose: { conn: any; promise: any } | undefined;
+  var mongoose:
+    | { conn: Connection | null; promise: Promise<Connection> | null }
+    | undefined;
 }
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/mydatabase";
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase';
 
-if(!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
+if (!MONGODB_URI) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env.local'
+  );
 }
 
-let catched = global.mongoose;
-if(!catched){
-    catched = global.mongoose = { conn: null, promise: null };
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
-export async function connectToDatabase() {
-    if(!catched) return;
-    if(catched.conn) return;
-    if(!catched.promise){
-        catched.promise = mongoose.connect(MONGODB_URI,{
-            bufferCommands: false,
-        });
-    }
-    catched.conn = await catched.promise;
-    return catched.conn;
+export async function connectToDatabase(): Promise<Connection | undefined> {
+  if (!cached) return;
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        bufferCommands: false,
+      })
+      .then((m) => m.connection);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
